70a6d93fe9dba42f4cae9d3f8e708e5161df34eb
Max.Thomas <max.thomas@hpi.de>
2024-09-03 14:07:28 +0200

    Initialize project

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ database/models.py
@@ -0,0 +1,60 @@
+"""
+Here the data models for the interaction with the database are stored.
+
+"""
+
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    age_group = db.Column(db.String(50))
+    gender = db.Column(db.String(20))
+    occupation = db.Column(db.String(100))
+
+    courses = db.relationship('Course', backref='user', lazy=True)
+
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    user_choice = db.Column(db.String(10), nullable=True)
+    start = db.Column(db.DateTime, nullable=True)
+    end = db.Column(db.DateTime, nullable=True)
+
+    keywords_gen = db.relationship('KeywordGen', secondary='keywords_gen_courses', backref='courses', lazy=True)
+    keywords_man = db.relationship('KeywordMan', secondary='keywords_man_courses', backref='courses', lazy=True)
+
+
+class KeywordGen(db.Model):
+    __tablename__ = 'keywords_gen'
+
+    id = db.Column(db.Integer, primary_key=True)
+    keyword = db.Column(db.String(255))
+
+
+class KeywordMan(db.Model):
+    __tablename__ = 'keywords_man'
+
+    id = db.Column(db.Integer, primary_key=True)
+    keyword = db.Column
+
+
+class KeywordGenCourse(db.Model):
+    __tablename__ = 'keywords_gen_courses'
+
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), primary_key=True)
+    keyword_id = db.Column(db.Integer, db.ForeignKey('keyword_gen.id'), primary_key=True)
+
+
+class KeywordManCourse(db.Model):
+    __tablename__ = 'keywords_man_courses'
+
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), primary_key=True)
+    keyword_id = db.Column(db.Integer, db.ForeignKey('keyword_man.id'), primary_key=True)
